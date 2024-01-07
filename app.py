d5c23cd8708abf88c3477e067e135119833c0d12
PratikChavhan99 <133641630+PratikChavhan99@users.noreply.github.com>
2024-01-07 14:41:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,39 @@
-Print ('Hello World')
-Print ("Hello India")
+pipeline {
+ environment {
+ imagename = “flashpratik/jenkins-docker”
+ registryCredential = ‘pratik-docckerhub’
+ dockerImage = ‘’
+ }
+ agent any
+ stages {
+ stage(‘Cloning Git’) {
+ steps {
+ git([url: ‘https://github.com/adithyak21/simple-docker.git', branch: ‘main’])
+ }
+ }
+ stage(‘Building image’) {
+ steps{
+ script {
+ dockerImage = docker.build imagename
+ }
+ }
+ }
+ stage(‘Running image’) {
+ steps{
+ script {
+ sh “docker run ${imagename}:latest”
+ }
+ }
+ }
+ stage(‘Deploy Image’) {
+ steps{
+ script {
+ docker.withRegistry( ‘’, registryCredential ) {
+ dockerImage.push(“$BUILD_NUMBER”)
+ dockerImage.push(‘latest’)
+ }
+ }
+ }
+ }
+ }
+}
